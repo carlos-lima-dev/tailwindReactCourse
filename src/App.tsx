4b89f1b194ec8f07e0938e9b1a573578b6adde68
catarina-das-merces <catarinadasmerces@outlook.pt>
2025-01-08 22:42:10 +0000

    add comment kate folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,31 @@
 import Borders from "./components/Borders/Borders";
 import Container from "./components/Container/Container";
-import {Flexbox} from "./components/Flexbox/Flexbox";
+import { Flexbox } from "./components/Flexbox/Flexbox";
 import Fontandcolors from "./components/Fontsandcolors/Fontandcolors";
 import Spacing from "./components/Spacing/Spacing";
 import WithandHeight from "./components/Withandheight/Withandheight";
 
 function App() {
-  return (
-    <>
-      <Container>
-        <Fontandcolors />
-      </Container>
-      <Container>
-        <Spacing />
-      </Container>
-      <Container>
-        <Borders />
-      </Container>
-      <Container>
-        <WithandHeight />
-      </Container>
-      <Container>
-        <Flexbox />
-      </Container>
-    </>
-  );
+	return (
+		<>
+			<Container>
+				<Fontandcolors />
+			</Container>
+			<Container>
+				<Spacing />
+			</Container>
+			<Container>
+				<Borders />
+			</Container>
+			<Container>
+				<WithandHeight />
+			</Container>
+			<Container>
+				<Flexbox />
+			</Container>
+			{/*add new comment*/}
+		</>
+	);
 }
 
 export default App;
